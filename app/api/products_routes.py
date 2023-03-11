05a654e23470aa7783fe0d687cbe1d417ba6e641
Clarence <clarencema@live.com>
2023-03-11 10:04:07 -0800

    created products backend route

---
+++ app/api/products_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import User, db
+from app.forms import LoginForm
+from app.forms import SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+products_routes = Blueprint('products', __name__)

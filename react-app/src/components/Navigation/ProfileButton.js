44f8a17b12a474c268dc6474a2e9aef0a3b07570
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-03-12 16:13:08 -0400

    nav bar completed

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
+import { NavLink } from 'react-router-dom';
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
@@ -38,36 +39,47 @@ function ProfileButton({ user }) {
   const closeMenu = () => setShowMenu(false);
 
   return (
-    <div>
+    <div className="profile-container">
       <button onClick={openMenu} className="nav-profile-button">
-          <i className="fas fa-user" />
-          <i className="fas fa-caret-down" />
+        <i className="fas fa-user" />
+        <i className="fas fa-caret-down" />
       </button>
       <div className={ulClassName} ref={ulRef}>
         {user ? (
-          <>
-            <div>{user.username}</div>
-            <div className="profile-dropdown">
-              Purchases
+          <div className="profile-dropdown-container">
+            <div className="profile-dropdown-div">
+              <div className="profile-dropdown-user">
+                <i className="fas fa-user" />
+                <span className="profile-dropdown-name">{user.username}</span>
+              </div>
+              <NavLink to="/orders" style={{ textDecoration: 'none', color: "black" }}>
+                <div className="profile-dropdown-purchases">
+                  <i className="fas fa-clipboard-list" />
+                  <span>Purchases</span>
+                </div>
+              </NavLink>
+              <div onClick={handleLogout} className="profile-dropdown-logout">
+                <i className="fas fa-sign-out-alt" />
+                <span className="profile-dropdown-logout-btn">Sign Out</span>
+              </div>
             </div>
-            <div>
-              <button onClick={handleLogout}>Log Out</button>
-            </div>
-          </>
+          </div>
         ) : (
-          <>
+          <div className="profile-logsign-div">
             <OpenModalButton
               buttonText="Log In"
               onItemClick={closeMenu}
+              modalClass="login-sign-btn"
               modalComponent={<LoginFormModal />}
             />
 
             <OpenModalButton
               buttonText="Sign Up"
               onItemClick={closeMenu}
+              modalClass="login-sign-btn"
               modalComponent={<SignupFormModal />}
             />
-          </>
+          </div>
         )}
       </div>
     </div>

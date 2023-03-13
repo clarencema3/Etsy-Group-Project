e8fe5164f8e0b84ea9a5075a1d5cc8b57ef877d0
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-03-13 16:39:21 -0400

    add to cart button functionality created

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts, fetchSingleProduct } from "../../store/products";
 import { useParams } from "react-router-dom";
 import './SingleProduct.css'
 import { clearState } from "../../store/products";
+import { createCartItem } from "../../store/cart";
 
 const SingleProduct = () => {
   const dispatch = useDispatch()
   const { productId } = useParams()
   const product = useSelector(state => state.products.product)
+  const user = useSelector(state => state.session.user)
+  const [quantity, setQuantity] = useState(0)
 
   const maxQuantity = [];
   for (let i = 1; i <= product?.stock; i++) {
@@ -20,6 +23,24 @@ const SingleProduct = () => {
     dispatch(fetchSingleProduct(productId))
     return () => dispatch(clearState())
   }, [dispatch, productId])
+
+  const onChangeHandler = (e) => {
+    setQuantity(e.target.value)
+  }
+
+  const addCartClick = async (e) => {
+    e.preventDefault()
+
+
+    const item_info = {
+      user_id: user.id,
+      product_id: product.id,
+      quantity: Number(quantity)
+    }
+
+    const newCartItem = await dispatch(createCartItem(item_info))
+  }
+
 
   if (!product) return <h1>loading</h1>
 
@@ -44,7 +65,7 @@ const SingleProduct = () => {
             <p>Rating</p>
             <p>Total Stock: {product.stock}</p>
             <p>Quantity</p>
-            <select className="select-quantity">
+            <select className="select-quantity" onChange={onChangeHandler}>
               <option>Select Quantity</option>
               {maxQuantity.map(number => (
                 <option>{number}</option>
@@ -53,7 +74,7 @@ const SingleProduct = () => {
             </select>
             <div className="button-div">
               <button className="buy-button">Buy it now</button>
-              <button className="add-cart-button">Add to Cart</button>
+              <button onClick={addCartClick} className="add-cart-button">Add to Cart</button>
             </div>
           </div>
         </div>
